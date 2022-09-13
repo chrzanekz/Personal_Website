6c0dfada9955cae1c7d10d613775003a0d7f27c3
chrzanekz <matix3010@gmail.com>
2022-09-13 20:00:10 +0200

    minor changes

--- tetris/tetris_java.js
+++ tetris/tetris_java.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded'() => {
+document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const width = 10

a40862d22c607a00e995b090e93bb1b8e55cebf0
chrzanekz <matix3010@gmail.com>
2022-09-13 21:52:53 +0200

    working

--- tetris/tetris_java.js
+++ tetris/tetris_java.js
@@ -2,7 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const width = 10
+    const ScoreDisplay= document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
 
-    console.log(squares)
+    //tetris shapes
 
+    const lTetromino= [
+        [1, width+1, width*2+1 2],
+        [width+3, width+4, width+5,width*2+5]
+    ]
+    
 })

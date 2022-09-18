330741eb4ce94ad150193fdc6133b6f0ef91ae9b
chrzanekz <matix3010@gmail.com>
2022-09-18 21:06:55 +0200

    adding some function like freeze and moving tetro

--- tetris/tetris_java.js
+++ tetris/tetris_java.js
@@ -60,6 +60,43 @@ document.addEventListener('DOMContentLoaded', () => {
       )
   }
 
-    draw()
+  
 
+  //undrawing the Tetromino
+
+  function undraw () {
+    current.forEach(index => {
+      squares[currentPosition+index].classList.remove('tetromino')
+    })
+  }
+
+
+// make the tetromino move every second downwards
+
+timerId = setInterval(moveDown, 500);
+
+//moveDown function
+
+function moveDown(){
+  undraw()
+  currentPosition += width
+  draw()
+  freeze()
+};
+
+// freeze function
+
+function freeze(){
+  if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+    current.forEach(index => squares[currentPosition+index].classList.add('taken'))
+      //start new tetromino falling
+      random = Math.floor(Math.random()*theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+  }
+}
+
+
+//end of function
 })
